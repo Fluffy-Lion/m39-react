b63d2449d5105ce6006af5d08908bf330a1c8d5c
Fluffy-Lion <leoncooper31@gmail.com>
2022-07-04 17:48:37 +0100

    day 1

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
-import './App.css'
+import "./App.css";
 
 let App = () => {
-  return (
-    <div>
-      <h1 className="heading">hello there</h1>
-      <Person 
-        name="leon" 
-        age="31"
-        petType="dog"
-        petName="pedro"
-      />
-      <Person
-        name="christian"
-        age="25"
-        petType="cat"
-        petName="tom"
-      />
-    </div>
-  )
+  return <div>app.js</div>;
 };
-
-let Person = (props) => {
-  return (
-    <div>
-      <h2 className="heading">my name is {props.name} i am {props.age} years old</h2>
-      <PetComponent petType={props.petType} petName={props.petName}/>
-    </div>
-  )
-}
-
-let PetComponent = (props) => {
-  return <p>i have a {props.petType} it's called {props.petName}</p>
-}
 export default App
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import Bbc from './Components/Bbc';
+import Bbc from './Day1/Bbc';
+import Day1App from './Day1/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
