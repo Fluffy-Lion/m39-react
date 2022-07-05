c045aaefd32b023bca45a828f75c2e788175f882
Fluffy-Lion <leoncooper31@gmail.com>
2022-07-05 15:13:31 +0100

    statejs update

--- src/Day2/State.js
+++ src/Day2/State.js
@@ -8,16 +8,23 @@ const State = () => {
   ]);
 
   const handleClick = (index) => {
+    // using spread syntax, spread the array into separate arguments
+    // https://medium.com/coding-at-dawn/how-to-use-the-spread-operator-in-javascript-b9e4a8b06fab
     let newArray  = [...persons]
+    // can change the value of our new array, in this case we are 
+    // removing the item from the 'index' position and deleting the 1 item
     newArray.splice(index, 1)
+    // updating 'persons' with the updated array
     setPersons(newArray)
   }
 
   return (
     <div>
       <h1>state functional component</h1>
+      {/* using the map method to iterate through my array */}
         {persons.map((person, index) => {
-            return <Person handleClick={handleClick} name={person.name} age={person.age} key={index}/>
+            // passing through props for each iteraterable item (person)
+            return <Person handleClick={() => handleClick(index)} name={person.name} age={person.age} key={index}/>
         })}
     </div>
   );
